81f5400e0680230c8f581a2ddfcd7cec798a7037
nardos <32473919+nardost@users.noreply.github.com>
2019-08-05 12:58:23 -0500

    Initial commit

---
+++ src/main/java/dev/nardos/springmvc101/SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package dev.nardos.springmvc101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcApplication.class, args);
+	}
+
+}
